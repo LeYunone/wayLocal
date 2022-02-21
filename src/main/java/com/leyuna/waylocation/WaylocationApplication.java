cd73fcf8ce22d8b91b9e18b5e90ea2d7aad2fd84
pengli <365627310@qq.com>
2022-02-21 09:33:01 +0800

    Initial commit

---
+++ src/main/java/com/leyuna/waylocation/WaylocationApplication.java
@@ -0,0 +1,13 @@
+package com.leyuna.waylocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaylocationApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(WaylocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leyuna/waylocation/WaylocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyuna.waylocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaylocationApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
