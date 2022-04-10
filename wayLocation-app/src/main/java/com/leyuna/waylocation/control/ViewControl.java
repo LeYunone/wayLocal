1f82c3782a373c900ccdbade1c3a558bff171fef
leyunone <365627310@qq.com>
2022-04-10 22:39:52 +0800

    [UPDATE]solve view conflict and pom,1.0.0version

---
+++ wayLocation-app/src/main/java/com/leyuna/waylocation/control/ViewControl.java
@@ -0,0 +1,20 @@
+package com.leyuna.waylocation.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author pengli
+ * @date 2022-04-10
+ */
+@Controller
+@RequestMapping("/")
+public class ViewControl {
+
+    @GetMapping("/methodtest")
+    public String htmlView(){
+        return "waylocation";
+    }
+}
--- waylocation-autoconfig/src/main/java/com/leyuna/waylocation/autoconfig/WaylocationAutoConfiguration.java
+++ waylocation-autoconfig/src/main/java/com/leyuna/waylocation/autoconfig/WaylocationAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.leyuna.waylocation.autoconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
