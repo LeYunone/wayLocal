65150a51abdd0c29c630014d44b7e32c3463c051
leyunone <365627310@qq.com>
2022-04-10 19:45:03 +0800

    [UPDATE]solve mybatis conflict

--- wayLocation-app/src/main/java/com/leyuna/waylocation/service/param/ParamService.java
+++ wayLocation-app/src/main/java/com/leyuna/waylocation/service/param/ParamService.java
@@ -4,6 +4,7 @@ import com.leyuna.waylocation.command.ParamExe;
 import com.leyuna.waylocation.response.DataResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
